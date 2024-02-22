048259138151b593e6d9ad94750845fde1f38c9f
Meredith <mbellomy81@gmail.com>
2024-02-21 23:25:37 -0500

    changes added to restapis.py for review

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -10,16 +10,26 @@ from requests.auth import HTTPBasicAuth
 #                                     auth=HTTPBasicAuth('apikey', api_key))
 
 def get_request(url, **kwargs):
-    print(kwargs)
+     # If argument contain API KEY
+    api_key = kwargs.get("api_key")
     print("GET from {} ".format(url))
     try:
-        # Call get method of requests library with URL and parameters
-        response = requests.get(
-            url, headers={"Content-Type": "application/json"}, params=kwargs
-        )
+        if api_key:
+            params = dict()
+            params["text"] = kwargs["text"]
+            params["version"] = kwargs["version"]
+            params["features"] = kwargs["features"]
+            params["return_analyzed_text"] = kwargs["return_analyzed_text"]
+            response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
+                                    auth=HTTPBasicAuth('apikey', api_key))
+        else:
+            # Call get method of requests library with URL and parameters
+            response = requests.get(url, headers={'Content-Type': 'application/json'},
+                                    params=kwargs)
     except:
         # If any error occurs
         print("Network exception occurred")
+
     status_code = response.status_code
     print("With status {} ".format(status_code))
     json_data = json.loads(response.text)
@@ -137,5 +147,21 @@ def get_dealer_reviews_from_cf(url, dealer_id):
 # - Call get_request() with specified arguments
 # - Get the returned sentiment label such as Positive or Negative
 
+def analyze_review_sentiments(dealerreview):
+    url = "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/c989295b-406a-43e5-992f-0742bcd9d389"
+    api_key = "W8RN3cMVOYg6ugb9VgY6ZbmaQnEcGtQowEC2VXEWNrVP"
+    authenticator = IAMAuthenticator(api_key)
+    natural_language_understanding = NaturalLanguageUnderstandingV1(version='2022-04-07',authenticator=authenticator)
+    natural_language_understanding.set_service_url(url)
+    try:
+        response = natural_language_understanding.analyze( text=dealerreview,features=Features(sentiment=SentimentOptions(targets=[dealerreview]))).get_result()
+        label=json.dumps(response, indent=2)
+        label = response['sentiment']['document']['label']
+
+        return(label)
+
+    except:
+        print("Can't analyze the sentiment")
+        return 'none'
 
 
