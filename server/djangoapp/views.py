8965ae431a4d978ee2eecb81335fae66548e69eb
Meredith <mbellomy81@gmail.com>
2024-02-26 15:35:29 -0500

    changes added to djangoapp

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -100,7 +100,7 @@ def registration_request(request):
 def get_dealerships(request):
     if request.method == "GET":
         context = {}
-        url = "https://mbellomy81-3000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get" 
+        url = "https://mbellomy81-3000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get" 
         dealerships = get_dealers_from_cf(url)
         context["dealership_list"] = dealerships
        
@@ -113,7 +113,7 @@ def get_dealerships(request):
 def get_dealer_details(request,dealer_id):
      if request.method == "GET":
          context = {}
-         dealer_url = "https://mbellomy81-3000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get"
+         dealer_url = "https://mbellomy81-3000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get"
          dealer = get_dealer_by_id_from_cf(dealer_url, id = dealer_id)
          context['dealer'] = dealer
 
